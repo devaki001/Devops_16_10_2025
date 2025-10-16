cc9cb0b98614fe2f854dd9135f8e2893babffb76
devaki001 <devikacv2005@gmail.com>
2025-10-16 09:33:54 +0530

    Create HelloWorld.java with a main method

---
+++ hello.java
@@ -0,0 +1,7 @@
+Create HelloWorld.java with:
+package com.example;
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println(&quot;Hello, DevOps CI/CD using GitHub Actions!&quot;);
+}
+}
